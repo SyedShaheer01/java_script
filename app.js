ef3d54feb093f51144b22567e3ed740e57e2415a
shaheer <syedshaheer266@gmail.com>
2023-02-14 08:57:29 -0800

    JS

---
+++ app.js
@@ -0,0 +1,78 @@
+// chapter 1:
+
+// q 1:
+
+// alert("")
+
+// q 2:
+
+// alert("hello world")
+
+// q 4:
+
+// alert("hello world")
+// alert("hello class")
+// alert("hello students")
+
+// q 5:
+
+// alert("hello i can run js through my browser console")
+
+// chapter 2:
+
+// q 1:
+
+// var username=""
+
+// q 2:
+
+// var myName="syed shaheer"
+
+// q 3:
+
+// var message="hello world"
+// alert(message);
+
+
+// q 4:
+
+// var student="jhone doe"
+//  var age="15 years old"
+// var skill="certificate mobile app development"
+// alert(student)
+// alert(age)
+// alert(skill)
+
+// q 6:
+// var email="example@example.com"
+// alert("my email address is"  + " "  + email)  
+
+
+// q 7:
+
+// var book="a smarter way to learn java script"
+// alert(book)
+
+
+// chapter 3:
+
+// q 1: 
+
+// var age="iam 20 years old"
+// alert(age)
+
+// q 2:
+
+// var visit="you have visited 5 times"
+// alert(visit);
+
+// q 3:
+// var birth="29 april"
+// console.log(birth)
+
+// q 4:
+
+var name="jhon doe"
+var product="shirts"
+var quantity="5"
+console.log(name + " " + "order" + " "   + quantity + " " + product)
