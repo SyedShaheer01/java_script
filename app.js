6f4991121383d54192bd5d8cfbbb803158b10d85
shaheer <syedshaheer266@gmail.com>
2023-02-16 08:37:52 -0800

    JS

--- app.js
+++ app.js
@@ -72,7 +72,190 @@
 
 // q 4:
 
-var name="jhon doe"
-var product="shirts"
-var quantity="5"
-console.log(name + " " + "order" + " "   + quantity + " " + product)
+// var name="jhon doe"
+// var product="shirts"
+// var quantity="5"
+// console.log(name + " " + "order" + " "   + quantity + " " + product)
+
+
+// chapter 4:
+
+
+// q 1:
+
+// var name="jhon",
+// userName="doe",
+// age=18;
+
+// document.getElementById("test").innerHTML= name + " " + userName + " " + age;
+// console.log(name,userName,age)
+
+
+// q 2:
+
+// illgel name:
+    // var user name=""
+    //  var rose="ok"
+    // var 10=""
+    
+    
+    // legal name:
+    // var userName=""
+    // var name=""
+    // var user_name="ok"
+    // var $name="ok"
+    //  console.log($name)
+
+    // chapter 5:
+    // q 1:
+
+
+//     var a=5;
+//     var b=3;
+
+// var sum=a+b;
+// console.log(sum)
+
+// q 2:
+// var a=5;
+//     var b=3;
+
+// var sum=a-b;
+// console.log(sum)
+
+
+
+// var a=5;
+//     var b=3;
+
+// var sum=a*b;
+// console.log(sum)
+
+
+// var a=5;
+//     var b=3;
+
+// var sum=a%b;
+// console.log(sum)
+
+
+// var a=5;
+//     var b=3;
+
+// var sum=a/b;
+// console.log(sum)
+
+// var a=5
+// var value=++a;
+// console.log(value)
+// var sum= value + 7;
+// console.log(sum)
+// var sub=sum-1;
+// console.log(sub)
+// var reminder=sub%3;
+// console.log(reminder);
+// var output=reminder;
+// console.log(output)
+
+
+// q 4:
+// var ticket=600
+// var value=600*5
+// console.log("total cost to buy 5 tickets is" +" " + value)
+
+
+// q 5:
+
+// var table=4*1 
+//  console.log("4*1=" + table)
+// var table=4*2
+// console.log("4*2=" + table)
+// var table=4*3
+// console.log("4*3=" + table)
+// table=4*4
+// console.log("4*4="+ table)
+// table=4*5
+// console.log("4*5="+ table)
+// table=4*6
+// console.log("4*6="+ table)
+// table=4*7
+// console.log("4*7="+ table)
+// table=4*8
+// console.log("4*8="+ table)
+// table=4*9
+// console.log("4*9="+ table)
+// table=4*10
+// console.log("4*10="+ table)
+
+// document.getElementById("test").innerHTML=(table +  + table1)
+
+// q 6:
+
+// var item1="price of item 1 is" +" " + 650;
+// console.log(item1)
+
+// var quantity1="quantity of item 1 is" + " " + 3;
+// console.log(quantity1);
+
+// var item2="price of item 2 is" + " " + 100;
+
+// var quantity2="quantity of item 2 is" + " " + 7;
+// console.log(quantity2)
+
+// var ship="shipping charges is" + " " + 100;
+// console.log(ship);
+
+// var total="total cost of your order is"+ " " + 2750;
+// console.log(total);
+
+// document.getElementById("test").innerHTML= (item1)
+// document.getElementById("book").innerHTML= (item2)
+
+// q 7:
+
+// var total="total marks =" + " " + 980;
+// console.log(total);
+
+// var obtain="obatain marks =" + " " + 804;
+// console.log(obtain)
+
+// var percantage=(804/980) *100;
+// console.log( "percantage:" + percantage)
+
+// q 9:
+
+// var a=  10
+// var b=25
+//  var sum=a*104.80 + b*28
+// console.log("total currency in pkr is" +" " + sum)
+// q 10:
+
+// var sum=5+5*(10/2);
+// console.log(sum)
+
+// q 11:
+
+
+// var year=  2016;
+// console.log(" current year:" + year)
+// var birth= 1992;
+// console.log("birth year" + birth);
+
+// var sum=year-birth;
+// console.log( "your age:" + sum)
+
+// q 11:
+var snack="chochlate chip"
+console.log("favourite snack:" + snack)
+
+var age=20
+console.log("current age:" + age)
+
+var maximum=60
+console.log("maximum age:" + maximum)
+
+var day=5
+console.log("snack per day:" + day)
+
+var sum=(maximum-age)*5;
+console.log("you will need 200 chochlate chip for rest of life:" +sum)
